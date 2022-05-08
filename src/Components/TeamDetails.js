c1b45c941b21e8d557da18ac1907286d3b5ee11b
mohammedsaif001 <mdsaif4online@gmail.com>
2022-05-08 20:08:15 +0530

    Changing loc of team images

--- src/Components/TeamDetails.js
+++ src/Components/TeamDetails.js
@@ -1,7 +1,8 @@
 const teamDetails = [
     {
         // image: 'https://media-exp1.licdn.com/dms/image/C5603AQFc7cHRZ4UIyQ/profile-displayphoto-shrink_800_800/0/1649163132894?e=1655337600&v=beta&t=uPczWuP4Ct2sLTKLIJpbcbAFfq3C-PsWkiLX6-pcGas',
-        image: process.env.PUBLIC_URL + "images/yamanappa.jpg",
+        // image: process.env.PUBLIC_URL + "images/yamanappa.jpg",
+        image: "images/yamanappa.jpg",
         personName: 'Prof Yamanappa',
         title: 'Assistant Professor - CSE',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta reprehenderit magni quas consequuntur neque ex quisquam! .',
@@ -12,7 +13,8 @@ const teamDetails = [
     },
 
     {
-        image: process.env.PUBLIC_URL + "images/abdullah.jpg",
+        // image: process.env.PUBLIC_URL + "images/abdullah.jpg",
+        image: "images/abdullah.jpg",
         personName: 'Mohammed Abdullah',
         title: '20181CSE0426',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta reprehenderit magni quas consequuntur neque ex quisquam! .',
@@ -22,7 +24,8 @@ const teamDetails = [
         instagram: 'https://www.instagram.com/_abdullah_02__/'
     },
     {
-        image: process.env.PUBLIC_URL + "images/nishithaa_photo.jpg",
+        // image: process.env.PUBLIC_URL + "images/nishithaa_photo.jpg",
+        image: "images/nishithaa_photo.jpg",
         personName: 'Nishitaa Palani',
         title: '20181CSE0490',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta reprehenderit magni quas consequuntur neque ex quisquam! .',
@@ -33,7 +36,8 @@ const teamDetails = [
     },
     {
         // image: 'https://media-exp1.licdn.com/dms/image/D4D03AQGGRqg7IOg_cg/profile-displayphoto-shrink_800_800/0/1637682265645?e=1654128000&v=beta&t=1qm2V282di62Rlh7-SixUC__bLS8QDiL7s0SP6gzZ-I',
-        image: process.env.PUBLIC_URL + "images/saif.jpg",
+        // image: process.env.PUBLIC_URL + "images/saif.jpg",
+        image: "images/saif.jpg",
         personName: 'Mohammed Saif',
         title: '20181CSE0433',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta reprehenderit magni quas consequuntur neque ex quisquam!.',
@@ -43,7 +47,8 @@ const teamDetails = [
         instagram: 'https://www.instagram.com/mohammedsaif001/'
     },
     {
-        image: process.env.PUBLIC_URL + "images/yunus.jpeg",
+        // image: process.env.PUBLIC_URL + "images/yunus.jpeg",
+        image: "images/yunus.jpeg",
         personName: 'Syed Yunus',
         title: '20181CSE0737 ',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta reprehenderit magni quas consequuntur neque ex quisquam!.',
@@ -54,7 +59,8 @@ const teamDetails = [
     },
 
     {
-        image: process.env.PUBLIC_URL + "images/uvais.jpg",
+        // image: process.env.PUBLIC_URL + "images/uvais.jpg",
+        image: "images/uvais.jpg",
         personName: 'Mohammed Uvais',
         title: '20181CSE0437',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta reprehenderit magni quas consequuntur neque ex quisquam!.',
