644b3da4d401069104d88b65fe9ebf568f1f6fab
mohammedsaif001 <mdsaif4online@gmail.com>
2022-05-10 22:21:51 +0530

    Ignore 2

--- src/Components/ModalNav.js
+++ src/Components/ModalNav.js
@@ -21,7 +21,7 @@ function ModalNav(props) {
                 <Modal.Body>
                     {/* <h4>Please Upload Tif File</h4> */}
                     <UploadImage />
-
+                    <ImageSliderCustom firstImage="https://static.zerochan.net/mery.%28dpqpqp550%29.full.1785984.jpg" secondImage="https://i.pinimg.com/474x/c3/76/47/c37647beb8c214c35bf59d950f1ea6a4.jpg" />
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={props.onHide}>Close</Button>
