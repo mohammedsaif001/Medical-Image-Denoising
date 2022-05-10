f996cc15c0a34a9aa41e46f8fd7182adf948c802
mohammedsaif001 <mdsaif4online@gmail.com>
2022-05-10 23:36:16 +0530

    removing slider from modal

--- src/Components/ModalNav.js
+++ src/Components/ModalNav.js
@@ -21,11 +21,6 @@ function ModalNav(props) {
                 <Modal.Body>
                     {/* <h4>Please Upload Tif File</h4> */}
                     <UploadImage />
-                    <div className="container text-center">
-
-                        <ImageSliderCustom firstImage="https://static.zerochan.net/mery.%28dpqpqp550%29.full.1785984.jpg" secondImage="https://i.pinimg.com/474x/c3/76/47/c37647beb8c214c35bf59d950f1ea6a4.jpg" />
-                        <div className="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi aspernatur ratione neque cum eveniet dolorum rerum, nulla, repellat molestiae nihil ut deserunt, culpa veritatis autem. Suscipit labore odit quae corporis, accusamus aspernatur sed soluta id officiis voluptatibus quidem dicta dolorum adipisci quia. Assumenda, earum facilis! Corporis distinctio tempora fuga sunt similique recusandae beatae quam?</div>
-                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={props.onHide}>Close</Button>
