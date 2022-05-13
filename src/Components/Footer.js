69c5fb78cf3bec5ac91c73baeed9cdd32dc50742
mohammedsaif001 <mdsaif4online@gmail.com>
2022-05-14 01:27:41 +0530

    Version 2.0.0

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className=''>
+        <div className='container'>
             <div className=" container-fluid-footer  footerbody">
                 {" "}
                 <output className="">
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark  fixed-top mb-3" style={{ background: 'black' }} id='navigation'>
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src="images/logo2.png" width="30" height="30" class="d-inline-block align-top" alt="" /> Medical Image Denoise</a>
+                <a className="navbar-brand" href="#"><img src="images/logo2.png" width="30" height="30" className="d-inline-block align-top" alt="" />  Medical Image Denoise</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
